4d482e4444a7495c479a6c1e9e8a43adcc7bb22d
Mason Bially <biallym@students.wwu.edu>
2012-05-17 14:35:30 -0700

    Cleaning up README. Incrementing version.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 ;; You can find most of OpenGL Java classes at:
 ;; <http://jogamp.org/deployment/jogamp-next/javadoc/jogl/javadoc/>
 
-(defproject photon "0.0.2"
+(defproject photon "0.0.4"
   :description "An idiomatic wrapper for OpenGL using JOGL."
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.clojure/core.incubator "0.1.0"]
